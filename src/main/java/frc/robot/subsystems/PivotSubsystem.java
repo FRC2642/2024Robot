8d13a318e4ca570f0bf4f214ab748cad2788dbbb
CommanderCoder15 <superzoom15.5@gmail.com>
2024-02-23 19:03:17 -0500

    added PivotSubsystem

---
+++ src/main/java/frc/robot/subsystems/PivotSubsystem.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class PivotSubsystem extends SubsystemBase {
+  /** Creates a new PivotSubsystem. */
+
+  private CANSparkMax pivotMotor = new CANSparkMax(0, MotorType.kBrushless);
+
+  public void setPivotSpeed(float pivotSpeed) {
+    pivotMotor.set(pivotSpeed);
+  }
+
+  public PivotSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
