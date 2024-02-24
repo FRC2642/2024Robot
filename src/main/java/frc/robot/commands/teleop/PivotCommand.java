668c25823d805c8cb0fa81cbd208dd5fd9f2671d
CommanderCoder15 <superzoom15.5@gmail.com>
2024-02-24 13:28:38 -0500

    idk what was changed

--- src/main/java/frc/robot/commands/teleop/PivotCommand.java
+++ src/main/java/frc/robot/commands/teleop/PivotCommand.java
@@ -28,7 +28,7 @@ public class PivotCommand extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-
+      
   }
 
   // Called once the command ends or is interrupted.
